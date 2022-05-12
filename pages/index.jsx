6ba4b2af80c5698a2ee48346b42f3bce62585719
victormicco <victor.oficial093@gmail.com>
2022-05-12 01:36:51 -0300

    Design Door

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
-import Presente from "../components/Presente";
+import Porta from "../components/Porta";
 
 export default function Home() {
   return (
-    <div>
-      <Presente />
+    <div style={{ display: "flex" }}>
+      <Porta selecionada />
+      <Porta />
     </div>
   );
 }
