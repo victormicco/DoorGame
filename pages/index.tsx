01f070cadbc395a8b0081503040e5f426a124bc3
victormicco <victor.oficial093@gmail.com>
2022-05-13 01:00:13 -0300

    Door events

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1));
 
   return (
-    <div style={{ display: "flex" }}>
-      <Porta porta={p1} />
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Porta value={p1} onChange={(novaPorta) => setP1(novaPorta)} />
     </div>
   );
 }
