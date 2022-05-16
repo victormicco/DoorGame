5916c75f67031a6445cd7a60f2fbd5fb164a62cb
victormicco <victor.oficial093@gmail.com>
2022-05-15 23:41:43 -0300

    End Game

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -1,11 +1,22 @@
-import styles from "../styles/Jogo.module.css";
-import { useState } from "react";
-import Porta from "../components/Porta";
-import { atualizarPortas, criarPortas } from "../functions/portas";
+import styles from "../../../styles/Jogo.module.css";
+import { useEffect, useState } from "react";
+import Porta from "../../../components/Porta";
+import { atualizarPortas, criarPortas } from "../../../functions/portas";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function jogo() {
-  const [portas, setPortas] = useState(criarPortas(5, 2));
+  const router = useRouter();
+
+  const [portas, setPortas] = useState([]);
+
+  useEffect(() => {
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+    setPortas(criarPortas(portas, temPresente));
+  }, [router?.query]);
+
+  console.log(router?.query);
 
   function renderizarPortas() {
     return portas.map((porta) => {
