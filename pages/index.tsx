14739237d9611ec4e67d71bcc61279f89e56f6d5
victormicco <victor.oficial093@gmail.com>
2022-05-16 22:12:37 -0300

    Prototype Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import Porta from "../components/Porta";
-import PortaModel from "../model/porta";
-import { atualizarPortas, criarPortas } from "./../functions/portas";
+import styles from "../styles/Formulario.module.css";
+import Cartao from "../components/Cartao";
+import Link from "next/link";
 
-export default function Home() {
+export default function Formulario() {
   return (
-    <div style={{ display: "flex" }}>
-      <h1>Formulário do Jogo</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   );
 }
