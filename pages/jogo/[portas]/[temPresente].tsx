426f6d8beabf7940c027390bdb28a31673500fa2
victormicco <victor.oficial093@gmail.com>
2022-05-16 23:11:37 -0300

    fix issue - Jogo component

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -5,7 +5,7 @@ import { atualizarPortas, criarPortas } from "../../../functions/portas";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function jogo() {
+export default function Jogo() {
   const router = useRouter();
 
   const [valido, setValido] = useState(false);
